9873c4baba536038d99f5c6062fb064f3121420a
Adam <adamskoullos@gmail.com>
2021-03-14 04:53:52 +0000

    login view and router

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <h3>Login</h3>
+      <input type="email" placeholder="Email" v-model="email" required>
+      <input type="password" placeholder="Password" v-model="password" required>
+      <div v-if="error" class="error">{{ error }}</div>
+      <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import useLogin from '@/composables/useLogin'
+
+export default {
+    setup(){
+        const email = ref('')
+        const password = ref('')
+
+        const { error, login } = useLogin()
+
+        const handleSubmit = async () => {
+            const res = await login(email.value, password.value)
+            if(!error.value){
+                // route to main view 
+            }
+        }
+
+        return { error, handleSubmit, email, password }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
